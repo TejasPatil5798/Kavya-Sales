ff175e11b4ca0e3a7d1a24c9fb883f3666f86b66
TejasPatil5798 <tejaspatil5jul@gmail.com>
2026-02-17 12:56:58 +0530

    updated

--- src/api/api.js
+++ src/api/api.js
@@ -1,15 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
-});
-
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("authToken");
-  if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
-  }
-  return req;
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? process.env.REACT_APP_API_URL
+      : "http://localhost:5000/api",
 });
 
 export default API;
