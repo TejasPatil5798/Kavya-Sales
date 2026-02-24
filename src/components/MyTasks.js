41498593c42a9daa49aaf96ae8bd38e4f7f06b8b
Tejas Patil <tejaspatil5jul@email.com>
2026-02-24 20:15:15 +0530

    finalizing UI

--- src/components/MyTasks.js
+++ src/components/MyTasks.js
@@ -219,19 +219,19 @@ const MyTasks = () => {
       {/* CARDS */}
       <div className="task-cards">
         <div className="kpi-card glass-card gradient-purple">
-          <p>Total Tasks</p>
+          <p style={{fontSize: "14px"}}>Total Tasks</p>
           <h3>{totalTasks}</h3>
         </div>
         <div className="kpi-card glass-card gradient-pink">
-          <p>Pending</p>
+          <p style={{fontSize: "14px"}}>Pending</p>
           <h3>{pendingTasks}</h3>
         </div>
         <div className="kpi-card glass-card gradient-teal">
-          <p>In Progress</p>
+          <p style={{fontSize: "14px"}}>In Progress</p>
           <h3>{inProgressTasks}</h3>
         </div>
         <div className="kpi-card glass-card gradient-orange">
-          <p>Completed</p>
+          <p style={{fontSize: "14px"}}>Completed</p>
           <h3>{completedTasks}</h3>
         </div>
       </div>
